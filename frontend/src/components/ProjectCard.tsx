b82bd3b172edf9ec7324d6342cd0bb143d10d0fb
Garett Takeyasu <takeyasu.garett@gmail.com>
2025-03-10 19:47:16 +0900

    feat: add ProjectCard to ProjectPage

--- frontend/src/components/ProjectCard.tsx
+++ frontend/src/components/ProjectCard.tsx
@@ -11,55 +11,54 @@ import {
   Badge,
 } from "@/components/ui";
 
-// Receive props from SessionPage component
-// SessionPage needs to handle fetch session data from the database
-// TO DO => interface needs to be put in a global file later? or not
-interface ProjectCardProps {
-  sessionData: {
+interface ProjectCardData {
+  projectData: {
     id: string;
     title: string;
     description: string;
     genre: string;
     contributors: string[];
-    dateCompleted: string;
+    dateCompleted: Date;
+    // reacts: react[] extended feature
   };
 }
 
-// TO DO => implement a way for the card to switch between sessions card to project card
-// based on if on SessionPage or ExplorePage
-const ProjectCard: React.FC<ProjectCardProps> = ({ sessionData }) => {
+// ProjectCards
+const ProjectCard: React.FC<ProjectCardData> = ({ projectData }) => {
   const navigate = useNavigate();
 
   return (
     <Card className='w-[350px] h-[250px] bg-background'>
+      {/* header of card => Genre, Title, Contributors */}
       <CardHeader>
         <div className='flex justify-between items-center'>
+          {/* TO DO change badge color based on genre, e.g. Horror = black background white text */}
           <Badge className='bg-indigo-100 text-indigo-800'>
-            {sessionData.genre}
+            {projectData.genre}
           </Badge>
           <div className='flex items-center gap-1'>
             <Users className='text-secondary-text h-4 w-4' />
             <span className='text-secondary-text text-sm'>
-              {sessionData.contributors.length}
+              {projectData.contributors.length}
             </span>
           </div>
         </div>
         <CardTitle className='text-primary-text text-left font-bold'>
-          {sessionData.title}
+          {projectData.title}
         </CardTitle>
       </CardHeader>
       <div>
         <CardDescription className='m-4 text-secondary-text'>
-          {sessionData.description}
+          {projectData.description}
         </CardDescription>
       </div>
       <CardFooter className='flex justify-between items-center'>
         <span className='text-sm text-secondary-text'>
-          Completed: {new Date(sessionData.dateCompleted).toLocaleDateString()}
+          Completed: {projectData.dateCompleted.toLocaleDateString()}
         </span>
         <Button
           className='bg-primary-button hover:bg-primary-button-hover'
-          onClick={() => navigate(`/projects/${sessionData.id}/read`)}
+          onClick={() => navigate(`/projects/${projectData.id}/read`)}
         >
           Read Story
         </Button>
