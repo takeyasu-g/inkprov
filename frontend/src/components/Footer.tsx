c2d4c29882c5abb54d80a43887df5d1001ed4edc
Garett Takeyasu <takeyasu.garett@gmail.com>
2025-03-18 17:40:17 +0900

    style: footer fixed?

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = function Footer() {
   return (
-    <footer className="flex justify-end relative bottom-0 left-0 w-full bg-white z-50 p-4 shadow-md">
+    <footer className="flex justify-end bottom-0 left-0 w-full bg-white z-50 p-4 shadow-md">
       {/* Side Links */}
       <div className="flex gap-4 items-center text-primary-text mr-6">
         <a href="/about" className="text-md hover:text-hover-text">
