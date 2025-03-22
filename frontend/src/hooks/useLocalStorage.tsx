9116b26e75ed83e31d844b12f9d110c39f580e11
Garett Takeyasu <takeyasu.garett@gmail.com>
2025-03-22 20:48:49 +0900

    feat: persistent input fields if refresh or leave page or logout. If sumbit form or cancel it will clear the localStorage

---
+++ frontend/src/hooks/useLocalStorage.tsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+
+// Modular custom hook, so that initial render gets localStorage data and not null, also taking care of updating localStorage
+// use this instead of useState if you want to use localStorage
+/**
+ *  @param {string} key => localStorage key for accessing the correct localStorage data (needs to be unique)
+ *  @param {T} initialValue => THIS NEEDS TO BE A OBJECT TO WORK (currently)
+ *  if localStorage is null then return this value
+ *  */
+
+// Can return type any as this custom hook will have many diffrent inputs
+function useLocalStorage<T>(
+  key: string,
+  initialValue: T
+): [T, React.Dispatch<React.SetStateAction<T>>] {
+  // Read from localStorage during initial render
+  const [value, setValue] = useState<T>(() => {
+    try {
+      const storedValue = localStorage.getItem(key);
+
+      if (storedValue) {
+        const parsedValue = JSON.parse(storedValue);
+
+        // merge with initalValue, if not all feilds were saved
+        return { ...initialValue, ...parsedValue };
+      } else {
+        // if null
+        return initialValue;
+      }
+    } catch (error) {
+      console.error("Error parsing localStorage data:", error);
+      return initialValue; // Fallback to initialValue if parsing fails
+    }
+  });
+
+  // Save to localStorage whenever value changes
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      localStorage.setItem(key, JSON.stringify(value));
+    }, 400); // Debounce for 300ms
+
+    return () => clearTimeout(timeoutId); // Cleanup on unmount or re-render
+  }, [key, value]);
+
+  return [value, setValue] as const;
+}
+
+export default useLocalStorage;
